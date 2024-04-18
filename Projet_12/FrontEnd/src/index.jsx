597ce37fe1ce54b72525ede988940fb1f90fa4cf
treisse93 <120468734+treisse93@users.noreply.github.com>
2024-04-18 11:07:26 +0200

    Delete Projet_12 directory

--- Projet_12/FrontEnd/src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import "./services/profileModel.js";
-
-const root = createRoot(document.getElementById("root")); // Utiliser createRoot directement
-root.render(<App />);
